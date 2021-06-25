6523f5fb0d13b93ea823b9343eae5b5d07178b86
Victor <49284171+victorfaarvang@users.noreply.github.com>
2021-06-26 01:08:05 +0200

    Remade wiktionary parsing
    Text will now output as such, and which sections to output can be chosen with the wanted_sections variable:
    Language:
    --- Section
    text from section
    --- Section
    text from section
    (repeat for all languages in response)
    
    I have disabled Chinese as the entire Chinese definitions are under a single section, the user would have to scroll too far to get past the Chinese definitions.

--- src/services/wikiService.js
+++ src/services/wikiService.js
@@ -28,15 +28,64 @@ async function lookup (query, locale = 'en') {
   const key = Object.keys(flat(body)).find(key => key.endsWith('.extract'));
   if (!key) return null; // 404 word not found
   const html = getProp(body, key);
-  const text = cheerio.load(html).text()
-    .trim() // remove extra whitespace and newlines
-    .replace('English\n', '')
-    .replace('Noun\n', '')
-    .replace('Etymology\n', 'Etymology: ')
-    .replace('Translation\n', 'Translation: ')
-    .replace('Anagrams\n', 'Anagrams: ')
-    .replace(/\n+/gm, '\n') // duplicate newlines
-    .replace(/\n/gm, '; ');
+  const $ = cheerio.load(html);
+
+  var langs = [];
+  var langs_indexes = [];
+
+  //The chinese section stores all information in a single section called 'definitions',
+  //which makes it unmanagable to include. Instead of skipping it entirely we could also
+  //show only the first x characters from the chinese 'definitions' section
+  const skip_languages = ['chinese']
+
+  //Select all language sections
+  const lang_select = $('h2');
+  for (let i = 0; i < lang_select.length; i++) {
+    if (!skip_languages.includes(lang_select.eq(i).text().trim().toLowerCase())) {
+      langs.push(lang_select.eq(i).text().trim());
+      langs_indexes.push(html.search(lang_select.eq(i).html())); 
+    }
+  }
+
+  //Go through each language section
+  var text = ""
+  for (let l = 0; l < langs.length; l++)
+  {
+    var lang_text = ""
+
+    const current_html = langs.length - 1 ? html.substring(langs_indexes[l],langs_indexes[l + 1]) : html.substring(langs_indexes[l],html.length);
+    const current_loaded = cheerio.load(current_html);
+
+    var sections = [];
+
+    //Select all minor sections
+    const section_selection = current_loaded('h3, h4, h5');
+    for (let i = 0; i < section_selection.length; i++) {
+      sections.push(section_selection.eq(i));
+    }
+  
+    const wanted_sections = ['kanji', 'noun', 'verb', 'adjective', 'particle', 'adverb', 'pronoun', 'conjunction', 'interjection', 'adnominal', 'numeral', 'number', 'usage notes', 'definitions'];
+    //Go through each minor section, and select the wanted ones
+    for (let i = 0; i < sections.length; i++) {
+      if (wanted_sections.includes(sections[i].text().trim().toLowerCase())) {
+        var current_section_html = null
+        if (i == sections.length - 1) {
+          current_section_html = current_html.substring(current_html.search(sections[i].html()), current_html.length);
+        }
+        else  {
+          current_section_html = current_html.substring(current_html.search(sections[i].html()), current_html.search(sections[i + 1].html()));
+        }
+        const a = cheerio.load(current_section_html);
+        lang_text += '--- ' + a.text().trim() + '\n';
+      }
+    }
+    if (lang_text.length != 0)
+    {
+      text += l == 0 ? langs[l] + ':\n' : '\n' + langs[l] + ':\n';
+      text += lang_text;
+    }
+  }
+
   return { query, html, text };
 }
 export default lookup;
