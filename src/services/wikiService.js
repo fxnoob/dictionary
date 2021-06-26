f5cec7f36e71d805b4aff856d9f1baf45302443e
Victor <49284171+victorfaarvang@users.noreply.github.com>
2021-06-26 12:39:20 +0200

    format fix

--- src/services/wikiService.js
+++ src/services/wikiService.js
@@ -14,7 +14,7 @@ async function lookup (query, locale = 'en') {
       action: 'query',
       format: 'json',
       prop: 'extracts',
-      origin: "*",
+      origin: '*',
       titles: query.toLowerCase()
     }
   });
@@ -26,7 +26,7 @@ async function lookup (query, locale = 'en') {
   });
   const body = await response.json();
   const key = Object.keys(flat(body)).find(key => key.endsWith('.extract'));
-  if (!key) return null; // 404 word not found
+  if (!key) return null // 404 word not found
   const html = getProp(body, key);
   const $ = cheerio.load(html);
 
@@ -36,24 +36,39 @@ async function lookup (query, locale = 'en') {
   //The chinese section stores all information in a single section called 'definitions',
   //which makes it unmanagable to include. Instead of skipping it entirely we could also
   //show only the first x characters from the chinese 'definitions' section
-  const skip_languages = ['chinese']
+  const skip_languages = ['chinese'];
 
   //Select all language sections
   const lang_select = $('h2');
   for (let i = 0; i < lang_select.length; i++) {
-    if (!skip_languages.includes(lang_select.eq(i).text().trim().toLowerCase())) {
-      langs.push(lang_select.eq(i).text().trim());
-      langs_indexes.push(html.search(lang_select.eq(i).html())); 
+    if (
+      !skip_languages.includes(
+        lang_select
+          .eq(i)
+          .text()
+          .trim()
+          .toLowerCase()
+      )
+    ) {
+      langs.push(
+        lang_select
+          .eq(i)
+          .text()
+          .trim()
+      );
+      langs_indexes.push(html.search(lang_select.eq(i).html()));
     }
   }
 
   //Go through each language section
-  var text = ""
-  for (let l = 0; l < langs.length; l++)
-  {
-    var lang_text = ""
+  var text = '';
+  for (let l = 0; l < langs.length; l++) {
+    var lang_text = '';
 
-    const current_html = langs.length - 1 ? html.substring(langs_indexes[l],langs_indexes[l + 1]) : html.substring(langs_indexes[l],html.length);
+    const current_html =
+      langs.length - 1
+        ? html.substring(langs_indexes[l], langs_indexes[l + 1])
+        : html.substring(langs_indexes[l], html.length);
     const current_loaded = cheerio.load(current_html);
 
     var sections = [];
@@ -63,24 +78,50 @@ async function lookup (query, locale = 'en') {
     for (let i = 0; i < section_selection.length; i++) {
       sections.push(section_selection.eq(i));
     }
-  
-    const wanted_sections = ['kanji', 'noun', 'verb', 'adjective', 'particle', 'adverb', 'pronoun', 'conjunction', 'interjection', 'adnominal', 'numeral', 'number', 'usage notes', 'definitions'];
+
+    const wanted_sections = [
+      'kanji',
+      'noun',
+      'verb',
+      'adjective',
+      'particle',
+      'adverb',
+      'pronoun',
+      'conjunction',
+      'interjection',
+      'adnominal',
+      'numeral',
+      'number',
+      'usage notes',
+      'definitions'
+    ];
     //Go through each minor section, and select the wanted ones
     for (let i = 0; i < sections.length; i++) {
-      if (wanted_sections.includes(sections[i].text().trim().toLowerCase())) {
-        var current_section_html = null
+      if (
+        wanted_sections.includes(
+          sections[i]
+            .text()
+            .trim()
+            .toLowerCase()
+        )
+      ) {
+        var current_section_html = null;
         if (i == sections.length - 1) {
-          current_section_html = current_html.substring(current_html.search(sections[i].html()), current_html.length);
-        }
-        else  {
-          current_section_html = current_html.substring(current_html.search(sections[i].html()), current_html.search(sections[i + 1].html()));
+          current_section_html = current_html.substring(
+            current_html.search(sections[i].html()),
+            current_html.length
+          );
+        } else {
+          current_section_html = current_html.substring(
+            current_html.search(sections[i].html()),
+            current_html.search(sections[i + 1].html())
+          );
         }
         const a = cheerio.load(current_section_html);
         lang_text += '--- ' + a.text().trim() + '\n';
       }
     }
-    if (lang_text.length != 0)
-    {
+    if (lang_text.length != 0) {
       text += l == 0 ? langs[l] + ':\n' : '\n' + langs[l] + ':\n';
       text += lang_text;
     }
