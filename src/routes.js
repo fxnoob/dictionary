dd81e9d266a7556540175eb904ba6c80f48daad2
Victor <49284171+victorfaarvang@users.noreply.github.com>
2021-06-26 16:17:06 +0200

    fixed bug that removes big portion of string and added multiple meanings

--- src/routes.js
+++ src/routes.js
@@ -33,7 +33,7 @@ const Routes = async () => {
           if (wikitionaryAllowed) {
             const wikiWord = await wikiService(term, langId);
             if (wikiWord) {
-              dicts = [{ word: term, meaning: wikiWord.text.split(".")[0] }];
+              dicts = [{ word: term, meaning: wikiWord.text, meanings: wikiWord.lang_meanings }];
             }
           }
         }
@@ -48,7 +48,6 @@ const Routes = async () => {
     try {
       // chrome.tts.speak(word, { lang: langId || 'en-US' });
       speechSynthesis(word, languageMap[langId] || 'en-US');
-      // eslint-disable-next-line no-empty
     } catch (e) {}
   });
 };
